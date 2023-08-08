bd551a525d75f5f89a7536de0d44f35a44f4a365
unknown <mjhonnyvieira@gmail.com>
2023-08-08 19:32:07 -0300

    Caça Níquel Robusto

---
+++ cacaniquel/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alunos
+ * Date: 26/04/2023
+ * Time: 21:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace cacaniquel
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(57, 166);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(100, 103);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Location = new System.Drawing.Point(163, 166);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(100, 103);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Location = new System.Drawing.Point(269, 166);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(100, 103);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 2;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(426, 97);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(183, 229);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox4.TabIndex = 3;
+			this.pictureBox4.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.OliveDrab;
+			this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button1.Location = new System.Drawing.Point(426, 97);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(183, 229);
+			this.button1.TabIndex = 4;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(95, 62);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(215, 42);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "R$ 1.000,00";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.OliveDrab;
+			this.ClientSize = new System.Drawing.Size(634, 401);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "MainForm";
+			this.Text = "cacaniquel";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ cacaniquel/MainForm.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alunos
+ * Date: 26/04/2023
+ * Time: 21:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace cacaniquel
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+
+			InitializeComponent();
+			
+		}
+	
+		Random rnd = new Random();
+		SoundPlayer sound = new SoundPlayer("alavanca.wav"); // Cria uma variavel para armazenar e possibilar a manipulação de um arquivo sonoro
+		int cont = 0; int n1, n2, n3; int money = 1000; // cont serve para armazenar o tempo do timer, n1 e semelhantes são os dados, money é money!
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			cont = 0; // Esse comando zera o contador sempre que o botão é pressionado
+			button1.Visible = false;
+			timer1.Enabled = true; // Ativa o timer
+			sound.Play(); // Começa a tocar o som, anteriormente declarado no public
+			pictureBox4.Load("lever5.gif"); // Sempre carrega o Gif quando pressionado
+			money = money - 10; label1.Text = "R$ "+ money.ToString() + ",00";
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			
+			cont++; // vai add 1 ao valor da variavel sempre que o timer atingir 100ml
+			
+			if(cont > 6) { // Esse if vai girar os dados
+				n1 = rnd.Next(1,7);
+				pictureBox1.Load("dado" + n1 + ".png");
+				n2 = rnd.Next(1,7);
+				pictureBox2.Load("dado" + n2 + ".png");
+				n3 = rnd.Next(1,7);
+				pictureBox3.Load("dado" + n3 + ".png");
+			}
+			
+			if(cont >= 30) { //Todos os outros ifs estão dentro destes! observe e pense sobre
+				
+				timer1.Enabled = false;
+				button1.Visible = true;
+			
+				if(n1 == n2 && n1 == n3) {
+					money += n1 * 50;
+					MessageBox.Show("Parabéns! Você cagou demais!!!\nRecebeu R$ " + n1 * 50 + ",00. Que sorte!");
+				}
+				
+				if(money == 0) {
+					MessageBox.Show("Putz... Não acredito man! Tu conseguiu ZERAR O GAME!!!\nPeço que reinicie pra continuar... XD");
+				}
+				
+				if(money == 3000) {
+					MessageBox.Show("Tá... Isso que é sorte! PARABÉNS! VOCÊ ALCANÇOU O LIMITE DE MONEY...\nEstá proibido de jogar, favor reinicie.");
+					button1.Visible = false;
+				}
+			}
+		}	
+	}
+}
